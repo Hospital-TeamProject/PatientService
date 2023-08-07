cb45412905c05940af91d803f3a0e495f074c63e
Mateja Musa <matejamusa6@gmail.com>
2023-08-07 20:46:39 +0200

    update AllergenRepository and AllergyRepository

--- src/main/java/com/hospital/PatientService/repository/AllergyRepository.java
+++ src/main/java/com/hospital/PatientService/repository/AllergyRepository.java
@@ -1,7 +1,17 @@
 package com.hospital.PatientService.repository;
 
 import com.hospital.PatientService.model.Allergy;
+import com.hospital.PatientService.model.HealthRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public interface AllergyRepository extends JpaRepository<Allergy, Long> {
+    Page<Allergy> findByHealthRecord(HealthRecord healthRecord, Pageable pageable);
+    @Modifying
+    @Query("update Allergy a set a.isDeleted = true where a.healthRecord=:healthRecord")
+    Integer deleteAllergyWithMatchingHeartRecord(@PathVariable HealthRecord healthRecord);
 }
