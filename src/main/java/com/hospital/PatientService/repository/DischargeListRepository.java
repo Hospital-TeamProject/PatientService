8aa251ef5c79b0720a5cb5676b0bdcc686ade673
MatejaMusa <matejamusa6@gmail.com>
2023-08-08 11:23:41 +0200

    update DischargeListRepository

--- src/main/java/com/hospital/PatientService/repository/DischargeListRepository.java
+++ src/main/java/com/hospital/PatientService/repository/DischargeListRepository.java
@@ -2,6 +2,7 @@ package com.hospital.PatientService.repository;
 
 import com.hospital.PatientService.model.DischargeList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface DischargeListRepository extends JpaRepository<DischargeList, Long> {
+public interface DischargeListRepository extends JpaRepository<DischargeList, Long>, JpaSpecificationExecutor<DischargeList> {
 }
