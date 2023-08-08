4f18993862e4e7829cd21accdefe32c05828122e
Mateja Musa <matejamusa6@gmail.com>
2023-08-08 16:36:21 +0200

    update HealthRecordRepository

--- src/main/java/com/hospital/PatientService/repository/HealthRecordRepository.java
+++ src/main/java/com/hospital/PatientService/repository/HealthRecordRepository.java
@@ -3,5 +3,8 @@ package com.hospital.PatientService.repository;
 import com.hospital.PatientService.model.HealthRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HealthRecordRepository extends JpaRepository<HealthRecord, Long> {
+    Optional<HealthRecord> findById(Long id);
 }
