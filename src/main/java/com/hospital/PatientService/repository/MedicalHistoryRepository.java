fd39e63f1a7057a00888437af414970afab645c2
Mateja Musa <matejamusa6@gmail.com>
2023-08-09 09:02:37 +0200

    update MedicalHistoryRepository

--- src/main/java/com/hospital/PatientService/repository/MedicalHistoryRepository.java
+++ src/main/java/com/hospital/PatientService/repository/MedicalHistoryRepository.java
@@ -1,7 +1,18 @@
 package com.hospital.PatientService.repository;
 
+import com.hospital.PatientService.model.HealthRecord;
 import com.hospital.PatientService.model.MedicalHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory, Long> {
+import java.util.List;
+
+public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory, Long>, JpaSpecificationExecutor<MedicalHistory> {
+    List<MedicalHistory> findByHealthRecord(HealthRecord healthRecord);
+
+    @Modifying
+    @Query("update MedicalHistory m set m.deleted=true where m.healthRecord=:healthRecord")
+    Integer updateDeletedAttributeByHealthRecord(HealthRecord healthRecord);
 }
