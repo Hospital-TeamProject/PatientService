2b8a8fa8c42f3ff803c2b8245a2bca857ad72d6c
Mateja Musa <matejamusa6@gmail.com>
2023-08-09 09:03:20 +0200

    update MedicalReportRepository

--- src/main/java/com/hospital/PatientService/repository/MedicalReportRepository.java
+++ src/main/java/com/hospital/PatientService/repository/MedicalReportRepository.java
@@ -2,6 +2,7 @@ package com.hospital.PatientService.repository;
 
 import com.hospital.PatientService.model.MedicalReport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface MedicalReportRepository extends JpaRepository<MedicalReport, Long> {
+public interface MedicalReportRepository extends JpaRepository<MedicalReport, Long>, JpaSpecificationExecutor<MedicalReport> {
 }
