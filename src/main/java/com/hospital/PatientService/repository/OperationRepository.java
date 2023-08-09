c10ecd188102f9767f4976c5b269b22c4c50404f
Mateja Musa <matejamusa6@gmail.com>
2023-08-09 09:08:19 +0200

    update OperationRepository

--- src/main/java/com/hospital/PatientService/repository/OperationRepository.java
+++ src/main/java/com/hospital/PatientService/repository/OperationRepository.java
@@ -1,7 +1,17 @@
 package com.hospital.PatientService.repository;
 
+import com.hospital.PatientService.model.HealthRecord;
 import com.hospital.PatientService.model.Operation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OperationRepository extends JpaRepository<Operation, Long> {
+    Page<Operation> findByHealthRecord(HealthRecord healthRecord, Pageable pageable);
+    @Modifying
+    @Query("update Operation o set o.isDeleted=true where o.healthRecord=:healthRecord")
+    Integer updateDeletedAttributeByHealthRecord(@Param("healthRecordId") HealthRecord healthRecord);
 }
