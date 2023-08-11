39baf2eded7223260490e9efc31e3977b08f2089
Mateja Musa <matejamusa6@gmail.com>
2023-08-11 20:45:49 +0200

    add param annotation to method in ScheduledTestingRepository

--- src/main/java/com/hospital/PatientService/repository/ScheduledTestingRepository.java
+++ src/main/java/com/hospital/PatientService/repository/ScheduledTestingRepository.java
@@ -5,6 +5,7 @@ import com.hospital.PatientService.model.ScheduledTesting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
@@ -12,5 +13,7 @@ import java.util.List;
 public interface ScheduledTestingRepository extends JpaRepository<ScheduledTesting, Long>, JpaSpecificationExecutor<ScheduledTesting> {
     @Query(value = "select s from ScheduledTesting s where s.patient = :patient " +
             "and s.dateAndTime>=:startDate and s.dateAndTime<=:endDate")
-    List<ScheduledTesting> findByPatientAndBetweenGivenDates(Patient patient, Date startDate, Date endDate);
+    List<ScheduledTesting> findByPatientAndBetweenGivenDates(@Param("patient") Patient patient,
+                                                             @Param("startDate") Date startDate,
+                                                             @Param("endDate") Date endDate);
 }
