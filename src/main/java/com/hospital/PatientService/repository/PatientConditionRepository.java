2fbe4833bdc04b97d1fb2f0936a506ea6f856046
Mateja Musa <matejamusa6@gmail.com>
2023-08-11 19:47:28 +0200

    update PatientConditionRepository

--- src/main/java/com/hospital/PatientService/repository/PatientConditionRepository.java
+++ src/main/java/com/hospital/PatientService/repository/PatientConditionRepository.java
@@ -2,6 +2,7 @@ package com.hospital.PatientService.repository;
 
 import com.hospital.PatientService.model.PatientCondition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PatientConditionRepository extends JpaRepository<PatientCondition, Long> {
+public interface PatientConditionRepository extends JpaRepository<PatientCondition, Long>, JpaSpecificationExecutor<PatientCondition> {
 }
