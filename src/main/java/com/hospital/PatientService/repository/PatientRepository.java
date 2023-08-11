9e7adbaccc11ca7360b8764d895038a60539ec62
Mateja Musa <matejamusa6@gmail.com>
2023-08-11 19:57:12 +0200

    update patientRepository

--- src/main/java/com/hospital/PatientService/repository/PatientRepository.java
+++ src/main/java/com/hospital/PatientService/repository/PatientRepository.java
@@ -2,6 +2,17 @@ package com.hospital.PatientService.repository;
 
 import com.hospital.PatientService.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PatientRepository extends JpaRepository<Patient, Long> {
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PatientRepository extends JpaRepository<Patient, Long>, JpaSpecificationExecutor<Patient> {
+    Optional<Patient> findByJmbg(String jmbg);
+
+    Optional<Patient> findByJmbgAndDeleted(String jmbg, boolean deleted);
+
+    Optional<Patient> findByPatientId(UUID patientId);
+
+    Optional<Patient> findByPatientIdAndDeleted(UUID patientId, boolean deleted);
 }
