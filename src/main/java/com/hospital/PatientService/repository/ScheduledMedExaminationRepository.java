0fb957611657e3974f8626c5cb621a4822e8310d
Mateja Musa <matejamusa6@gmail.com>
2023-08-11 20:44:26 +0200

    update ScheduledMedExaminationRepository and ScheduledTestingRepository

--- src/main/java/com/hospital/PatientService/repository/ScheduledMedExaminationRepository.java
+++ src/main/java/com/hospital/PatientService/repository/ScheduledMedExaminationRepository.java
@@ -15,13 +15,15 @@ import java.util.UUID;
 public interface ScheduledMedExaminationRepository extends JpaRepository<ScheduledMedExamination, Long>, JpaSpecificationExecutor<ScheduledMedExamination> {
     Optional<ScheduledMedExamination> findByDoctorId(UUID doctorId);
 
-    @Query(value = "select s from ScheduledMedExamination s where s.doctorId = :doctorId and s.appointmentDate = :appointmentDate")
+    @Query(value = "select s from ScheduledMedExamination s where s.doctorId = :doctorId " +
+            "and s.appointmentDate>=:startDate and s.appointmentDate<=:endDate")
     Optional<List<ScheduledMedExamination>> findSchedMedExamByDoctorIdAndBetweenGivenDates(@Param("doctorId") UUID doctorID,
                                                                                            @Param("appointmentDate") Date startDate,
                                                                                            @Param("endDate") Date endDate);
 
 
-    @Query(value = "select s from ScheduledMedExamination s where s.patient = :patient and s.appointmentDate = :appointmentDate")
+    @Query(value = "select s from ScheduledMedExamination s where s.patient = :patient" +
+            " and s.appointmentDate>=:startDate and s.appointmentDate<=:endDate")
     Optional<List<ScheduledMedExamination>> findSchedMedExamByPatientAndBetweenGivenDates(@Param("patient") Patient patient,
                                                                                            @Param("appointmentDate") Date startDate,
                                                                                            @Param("endDate") Date endDate);
--- src/main/java/com/hospital/PatientService/repository/ScheduledTestingRepository.java
+++ src/main/java/com/hospital/PatientService/repository/ScheduledTestingRepository.java
@@ -1,7 +1,16 @@
 package com.hospital.PatientService.repository;
 
+import com.hospital.PatientService.model.Patient;
 import com.hospital.PatientService.model.ScheduledTesting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ScheduledTestingRepository extends JpaRepository<ScheduledTesting, Long> {
+import java.util.Date;
+import java.util.List;
+
+public interface ScheduledTestingRepository extends JpaRepository<ScheduledTesting, Long>, JpaSpecificationExecutor<ScheduledTesting> {
+    @Query(value = "select s from ScheduledTesting s where s.patient = :patient " +
+            "and s.dateAndTime>=:startDate and s.dateAndTime<=:endDate")
+    List<ScheduledTesting> findByPatientAndBetweenGivenDates(Patient patient, Date startDate, Date endDate);
 }
